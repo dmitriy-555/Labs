ec5622f6de181892643acb266761302f927bd0d5
Dmitriy <AdamDenI2021@gmail.com>
2024-04-20 13:29:44 +0300

    Добавьте файлы проекта.

---
+++ lab27/lab27.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <list>
+
+using namespace std;
+
+class Complex {
+private:
+    double a, b;
+public:
+    Complex(double z = 0.0, double y = 0.0) {
+        a = z;
+        b = y;
+    }
+    double i() {
+        return b;
+    }
+    void print() {
+        if (b < 0) cout << a << " " << b << "i";
+        else if (b > 0) cout << a << " + " << b << "i";
+        else cout << a;
+        cout << endl;
+    }
+};
+
+int main()
+{
+    list<Complex> list{
+        Complex(1, 2),
+        Complex(0.5, 6),
+        Complex(9, -4),
+        Complex(-4, 5.7),
+        Complex(0, 0)
+    };
+
+    for (auto iter = list.begin(); iter != list.end();)
+    {
+        if ((*iter).i() < 0) {
+            iter = list.erase(iter);
+        }
+        else {
+            ++iter;
+        }
+    }
+
+    for (auto iter = list.begin(); iter != list.end(); iter++)
+    {
+        (*iter).print();
+    }
+
+    return 0;
+}
