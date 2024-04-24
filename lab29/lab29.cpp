9a83245bb4c7cee433c7d8c8eaead17ec93738b6
Dmitriy <AdamDenI2021@gmail.com>
2024-04-24 20:03:29 +0300

    Добавьте файлы проекта.

---
+++ lab29/lab29.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <set>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    set<char> set;
+    
+    string seq;
+    getline(cin, seq);
+
+    for (char str : seq) {
+        if (ispunct(str)) {
+            set.insert(str);
+        }
+
+    }
+
+    for (char str : set) {
+        cout << str << " ";
+    }
+
+    return 0;
+}
+
